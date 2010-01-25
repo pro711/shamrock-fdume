72121740ddc3817fc635480eb931e2e01bd92655
Pro711 <Pro711@647b9960-05ae-11df-8c27-7d37615f5189>
2010-01-25 09:24:38 +0000

    * implements book detail view

--- trunk/apps/book/urls.py
+++ trunk/apps/book/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('apps.book.views',
     (r'^$', 'book_index'),
     (r'^add/$', 'book_add'),
+    (r'^(?P<book_id>\d+)/$', 'book_detail'),
 #    (r'^create/$', 'add_person'),
 #    (r'^show/(?P<key>.+)$', 'show_person'),
 
--- trunk/apps/book/views.py
+++ trunk/apps/book/views.py
@@ -6,6 +6,8 @@ from django.core.paginator import Paginator, InvalidPage
 from django.views.generic.list_detail import object_list, object_detail
 from django.views.generic.create_update import create_object, delete_object, \
     update_object
+from django import forms as djangoforms
+
 from google.appengine.ext import db
 from google.appengine.ext.db import djangoforms as forms
 from mimetypes import guess_type
@@ -18,9 +20,10 @@ from ragendja.dbutils import get_object_or_404
 from ragendja.template import render_to_response
 
 class BookAddForm(forms.ModelForm):
+    description = djangoforms.CharField(widget=djangoforms.Textarea,required=False)
     class Meta:
         model = BookItem          
-        #exclude = ['post_date', ]
+        exclude = ['book_id', 'post_date', ]
 
 
 
@@ -30,7 +33,10 @@ def book_index(request):
     q.order("-post_date")
     results = q.fetch(8)
     #FIXME: odd numbers fetched
-    items = [results[2*n:2*n+2] for n in range(len(results)/2)]
+    num = len(results)
+    items = [results[2*n:2*n+2] for n in range(num/2)]
+    if num < 8 and (num%2 == 1) :
+        items.append([results[-1],])
     
     template_values = {
             'books' : items,
@@ -47,10 +53,49 @@ def book_add(request):
         return render_to_response(request, "book/book_add.html", template_values)
     else:
         form = BookAddForm(request.POST)
+        
         if form.is_valid():
-            post = form.save(commit=False)
-            post.put()
+            entity = form.save(commit=False)
+            
+            # Set valid_date
+            
+            # Auto-increment book_id, starting from 1000
+            q = BookItem.all()
+            q.order("-book_id")
+            results = q.fetch(1)
+            if len(results) == 0:
+                last_id = 1000
+            else:
+                last_id = results[0].book_id
+            entity.book_id = last_id + 1
+            
+            entity.put()
             return HttpResponseRedirect("/book/")
+        else:
+            template_values = {
+                    'form' : form,
+                    }
+            return render_to_response(request, "book/book_add.html", template_values)
+
+
+def book_detail(request, book_id):
+    q = BookItem.all()
+    q.filter("book_id =", long(book_id))
+    results = q.fetch(1)
+    
+    if len(results) == 0:
+        raise Http404, "Book of id %s not found." % book_id
+    else:
+        template_values = {
+                'e' : results[0],
+                }
+        return render_to_response(request, "book/book_detail.html", template_values)
+
+
+
+
+
+
 
 
 
