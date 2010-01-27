4893f0d35f1ec850e0d08805248d07ecbf8a258d
Pro711 <Pro711@647b9960-05ae-11df-8c27-7d37615f5189>
2010-01-27 07:04:19 +0000

    * implements add book from douban view

--- trunk/apps/book/urls.py
+++ trunk/apps/book/urls.py
@@ -4,8 +4,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('apps.book.views',
     (r'^$', 'book_index'),
     (r'^add/$', 'book_add'),
+    (r'^add_from_douban/$', 'book_add_douban'),
     (r'^(?P<book_id>\d+)/$', 'book_detail'),
-#    (r'^create/$', 'add_person'),
-#    (r'^show/(?P<key>.+)$', 'show_person'),
 
 )
