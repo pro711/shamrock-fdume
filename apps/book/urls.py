f8e35111c18e60217e1f3b25bc0d621f0ba9f7a0
lighteningman <lighteningman@647b9960-05ae-11df-8c27-7d37615f5189>
2010-02-28 15:54:20 +0000

    sold tab

--- apps/book/urls.py
+++ apps/book/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('apps.book.views',
     (r'^add/$', 'book_add'),
     (r'^add_from_douban/$', 'book_add_douban'),
     (r'^(?P<book_id>\d+)/$', 'book_detail'),
+    (r'^(?P<book_id>\d+)/mark_as_sold/$', 'book_mark_as_sold'),
     (r'^error/(?P<error_id>\d+)/$', 'book_error'),
 	(r'^item_search/$','book_search')
 )
